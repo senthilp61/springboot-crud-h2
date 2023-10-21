fe9ad15251f8aeca290ca4160dbd4ef28893e5f5
Senthil Prakash P <senthilp61@gmail.com>
2023-10-21 22:27:20 +0530

    first commit with project structure

---
+++ src/main/java/com/or/learning/cruddemoh2/CrudDemoH2Application.java
@@ -0,0 +1,13 @@
+package com.or.learning.cruddemoh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/or/learning/cruddemoh2/CrudDemoH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.or.learning.cruddemoh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
